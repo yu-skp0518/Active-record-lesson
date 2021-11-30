5c9bc2cff791f235a484ea97da97f79cdd51485c
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-11-30 15:53:40 +0000

    Data extraction

--- main.rb
+++ main.rb
@@ -47,11 +47,41 @@ end
 # --------------------------------------------------
 
 ## insert_2
-user = User.new do |u|
-  u.name = "mochizuki"
-  u.age = 18
-end
+# user = User.new do |u|
+#   u.name = "mochizuki"
+#   u.age = 18
+# end
 
-user.save
+# user.save
 
 # --------------------------------------------------
+
+## データの抽出
+
+#毎回データを作成されないようにまずデータを全削除する
+User.delete_all
+
+# i = 0
+# 5.times do
+#   i += 1
+#   User.create(id: i, name: "test#{i}", age: "2#{i}".to_i )
+# end
+
+# User.create
+User.create(name: "tanaka", age: 19)
+User.create(name: "takahashi", age: 25)
+User.create(name: "hayashi", age: 31)
+User.create(name: "mizutani", age: 28)
+User.create(name: "otsuka", age: 35)
+
+##データ抽出
+# pp User.all
+# pp User.select(:id, :name, :age).all
+# pp User.select(:id, :name, :age).first
+# pp User.select(:id, :name, :age).last
+
+#最初の三件のみ
+# pp User.select(:id, :name, :age).first(3)
+
+#idと名前のみ
+pp User.select(:id, :name).all
