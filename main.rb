2694b261b6af567f4e513b9c6699b77491da5fcb
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-12-01 01:53:21 +0000

    Data extraction-2

--- main.rb
+++ main.rb
@@ -74,7 +74,9 @@ User.create(name: "hayashi", age: 31)
 User.create(name: "mizutani", age: 28)
 User.create(name: "otsuka", age: 35)
 
-##データ抽出
+# --------------------------------------------------
+
+##データ抽出_1
 # pp User.all
 # pp User.select(:id, :name, :age).all
 # pp User.select(:id, :name, :age).first
@@ -84,4 +86,26 @@ User.create(name: "otsuka", age: 35)
 # pp User.select(:id, :name, :age).first(3)
 
 #idと名前のみ
-pp User.select(:id, :name).all
+# pp User.select(:id, :name, :age).all
+
+# --------------------------------------------------
+
+##データ抽出_2
+
+#findメソッドはidで探す場合のみ
+# pp User.find(3)
+
+#さらにフィールドを指定したい場合はselectメソッド(:id, :name, :age)のような書き方もできる
+# pp User.select("id, name, age").find(3)
+
+#idではなく他の値で検索したい場合はfind_byメソッドを使う(find_byの後にはfind_by_nameのようにカラム名を続けても使用することができる)
+#下の4行は全て同じ実行結果になる
+# pp User.select("id, name, age").find_by(name: "tanaka")
+# pp User.select("id, name, age").find_by name: "tanaka" #Rubyでは括弧を省略することもできる
+# pp User.select("id, name, age").find_by_name "tanaka" #find_by_nameのようにカラム名をつなげて使用することもできる
+# pp User.select("id, name, age").find_by_name("tanaka")
+
+#存在しないレコードを抽出した場合
+pp User.select("id, name, age").find_by_name("kiriya")
+#nilではなくエラーで返してほしい時はメソッド名の後に!をつける
+pp User.select("id, name, age").find_by_name!("kiriya")
