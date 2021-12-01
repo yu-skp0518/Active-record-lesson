5d8912a0387df965bdeb73d52eb8531e67903b7d
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-12-01 02:57:33 +0000

    find,where,and,not - method

--- main.rb
+++ main.rb
@@ -77,7 +77,9 @@ User.create(name: "otsuka", age: 35)
 # --------------------------------------------------
 
 ##データ抽出_1
-# pp User.all
+## - select
+
+pp User.all
 # pp User.select(:id, :name, :age).all
 # pp User.select(:id, :name, :age).first
 # pp User.select(:id, :name, :age).last
@@ -91,6 +93,7 @@ User.create(name: "otsuka", age: 35)
 # --------------------------------------------------
 
 ##データ抽出_2
+## - find
 
 #findメソッドはidで探す場合のみ
 # pp User.find(3)
@@ -106,6 +109,36 @@ User.create(name: "otsuka", age: 35)
 # pp User.select("id, name, age").find_by_name("tanaka")
 
 #存在しないレコードを抽出した場合
-pp User.select("id, name, age").find_by_name("kiriya")
-#nilではなくエラーで返してほしい時はメソッド名の後に!をつける
-pp User.select("id, name, age").find_by_name!("kiriya")
+# pp User.select("id, name, age").find_by_name("kiriya")
+# #nilではなくエラーで返してほしい時はメソッド名の後に!をつける
+# pp User.select("id, name, age").find_by_name!("kiriya")
+
+# --------------------------------------------------
+
+##データ抽出_3
+## - where
+
+#20代のレコード
+# pp User.select("id, name, age").where(age: 20..29)
+#値をピンポイントで指定することもできる
+# pp User.select("id, name, age").where(age: [19, 31])
+
+
+## - AND (whereを繋いで使用する)
+
+# pp User.select("id, name, age").where("age >= 20").where("age < 30")
+# #もしくは括弧内でandを使う
+# pp User.select("id, name, age").where("age >= 20 and age < 30")
+
+
+## - OR (whereを繋いで使用する)
+#括弧内でorを使う
+pp User.select("id, name, age").where("age <= 20 or age >= 30")
+#もしくはorメソッドとして使用する(その際はor以下に同じ構造を持たなければいけない)
+pp User.select("id, name, age").where("age <= 20").or(User.select("id, name, age").where("age >= 30"))
+#またはselectメソッドを文末にもってくる
+pp User.where("age <= 20").or(User.where("age >= 30")).select("id, name, age")
+
+
+## - NOT
+pp User.select("id, name, age").where.not(id: 3)
